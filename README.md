8dcf56a48f42af7dfd21a3f3aa1282de547b4f89
JZ <jitendra.zaa@gmail.com>
2025-03-02 21:14:22 -0500

    Command making external API Call

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # sf_workbook - Salesforce DX Plugin for Configuration Workbook
 
-## How to run the plugin
+## Sample Plugin Commands
 
 1. `bin/dev.js jz wbook --name Astro`
+1. `sf call external service` - This command make API call and provides some interesting facts about numbers
 
 ## Important Links
-
 - [How to create plugin documentation](https://github.com/salesforcecli/cli/wiki/Get-Started-And-Create-Your-First-Plug-In)
 
 ## Troubleshoot
-
 1. If changes in plugin is not reflecting, that means after changes code is not compiled. Run this command in seperate terminal , so that after each file change code automatically compiled. `yarn compile --watch`
+1. Remove Husky checks before git commit , else we would not be able to commit code in git `rm -rf .husky/`
---
+++ src/commands/call/external/service.ts
@@ -0,0 +1,40 @@
+
+
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import { Messages } from '@salesforce/core';
+import got from 'got';
+
+Messages.importMessagesDirectoryFromMetaUrl(import.meta.url)
+const messages = Messages.loadMessages('sf_workbook', 'call.external.service');
+
+export type CallExternalServiceResult = {
+  text: string;
+  number: number;
+  found: boolean;
+  type: string;
+};
+
+export default class CallExternalService extends SfCommand<CallExternalServiceResult> {
+  public static readonly summary = messages.getMessage('summary');
+  public static readonly description = messages.getMessage('description');
+  public static readonly examples = messages.getMessages('examples');
+
+  public static readonly flags = {
+    name: Flags.string({
+      summary: messages.getMessage('flags.name.summary'),
+      description: messages.getMessage('flags.name.description'),
+      char: 'n',
+      required: false,
+    }),
+  }
+
+  public async run(): Promise<CallExternalServiceResult> {
+    const result = await got<CallExternalServiceResult>(
+      'http://numbersapi.com/random/trivia?json'
+    ).json<CallExternalServiceResult>();
+
+    this.log(result.text);
+
+    return result;
+}
+}
